8d6534fa478578ed390a51673d644724662a8b00
Pdysted <pdyst@DESKTOP-N9B5C76>
2016-05-09 11:29:48 +0200

    dasd

--- view/CasePanel.java
+++ view/CasePanel.java
@@ -100,7 +100,7 @@ public class CasePanel extends javax.swing.JPanel {
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(konsNrLabel)
-                        .addContainerGap(51, Short.MAX_VALUE))
+                        .addContainerGap(497, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(casePanelEditButton)
