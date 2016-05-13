877c42bbcc0503d8b11195a23295a86284f57dd0
Pdysted <pdyst@DESKTOP-N9B5C76>
2016-05-13 13:37:01 +0200

    10101010101000110101010

--- listeners/SearchListListener.java
+++ listeners/SearchListListener.java
@@ -25,7 +25,7 @@ public class SearchListListener implements KeyListener {
     }
     @Override
     public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
