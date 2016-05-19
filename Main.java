71632cbcbc5ee9e890cb662eb36cbff32d63ec05
Pdysted <pdyst@DESKTOP-N9B5C76>
2016-05-19 08:47:54 +0200

    10101010101000110101010 prepared statement

--- Main.java
+++ Main.java
@@ -21,28 +21,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Først åbnes et vindue/client, hvor man logger ind,
-        //Hvorefter EmployeeHandler henter en employee fra databasen,
-        //Som ud fra admin og partime opretter control ud fra en af følgende controls sub-classes:
-        //AdminControl, EmployeeControl og PartTimeControl således:
-        //Logind skærm åbnes her
-//        employee = EmployeeHandler.getInstance().getEmployee(username, password);
-//        if (employee.isAdmin()) {
-        //control = new AdminControl()
-        //}
-        //GUI gui = new GUI(control); 
-        
         LoginView lw = new LoginView();
         lw.setVisible(true);
     }
         
-//    
-//    public void login() throws SQLException{
-//        
-//        employee = EmployeeHandler.getInstance().getEmployee("dasda", "dsa");
-//        if (employee.isAdmin()) {
-//            control = new AdminControl();
-//        }
-//        }
-//    
 }
